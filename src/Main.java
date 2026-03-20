21097539bfc56448ce69fe944377522b49f32754
impichler <impichler@thewildcats.net>
2026-03-20 13:53:24 -0400

    Added in scanner and operator logic

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,45 @@ import java.util.Scanner;
 //
 
 public class Main{
-    public static void main (String[] args){
-        CalculatorController controller = new CalculatorController();
+    public static void main (String[] args) {
+        // CalculatorController controller = new CalculatorController();
+
+        // initializes user interface
+        Scanner scanner = new Scanner(System.in);
+        //creates a loop
+        boolean whileCalc = true;
+
+        //everything in while loop happens when whileCalc is true
+        while (whileCalc) {
+
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Please enter a operation");
+            String operator = scanner.next();
+
+            double result = 0;
+
+            // operator logic
+            switch (operator){
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+        }
 
     }
-    // creates a user interface
-    Scanner userInput = new Scanner(System.in);
-    //
-    double int1 = userInput.nextDouble();
-    double int2 = userInput.nextDouble();
-    double result = 0;
-
-
 }
