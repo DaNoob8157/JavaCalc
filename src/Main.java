be9137aad4d35f4c694b6ec5b3e4884e45283840
Alexander McKelvey <sashapmckelvey@gmail.com>
2026-03-20 12:01:21 -0400

    Update Main.java
    Creates a terminal user interface
    
    Co-Authored-By: Iz <253328155+Phantom-StormX@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ import java.util.Scanner;
 public class Main{
     public static void main (String[] args){
         CalculatorController controller = new CalculatorController();
+
     }
+    // creates a user interface
+    Scanner userInput = new Scanner(System.in);
+    //
+    double int1 = userInput.nextDouble();
+    double int2 = userInput.nextDouble();
+    double result = 0;
+
+
 }
